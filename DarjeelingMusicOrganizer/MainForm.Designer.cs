38bf0b78b233983437f7693d23d128ca383a962a
joshuazhang447 <w0863366@myscc.ca>
2026-01-28 23:16:31 -0500

    Project Setup:
    Migrate to MainForm with WebView2 and added the Vite React app
    
    Changed Form1 file to MainForm and hosting a WebView2 browser that loads the new React frontend served via Vite at "http://localhost:5173".
    
    Added Microsoft.Web.WebView2 NuGet package.
    
    Set up the React+Vite+Tailwind project with ESLint, PostCSS, and Tailwind
    
    Updated the .gitignore for the Node/Vite

--- DarjeelingMusicOrganizer/MainForm.Designer.cs
+++ DarjeelingMusicOrganizer/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DarjeelingMusicOrganizer
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -22,16 +22,13 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1000, 800);
+            this.Text = "Darjeeling Music Organizer";
         }
 
         #endregion
---
+++ DarjeelingMusicOrganizer/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DarjeelingMusicOrganizer
+{
+    public partial class MainForm : Form
+    {
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            _ = InitializeWebViewAsync();
+        }
+
+        private async Task InitializeWebViewAsync()
+        {
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.webView.Dock = DockStyle.Fill;
+            this.Controls.Add(this.webView);
+            await this.webView.EnsureCoreWebView2Async(null);
+            this.webView.Source = new Uri("http://localhost:5173");
+        }
+    }
+}
